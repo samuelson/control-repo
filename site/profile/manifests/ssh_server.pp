2ced6c2d6736ee29a3c4e4e0bae6f72cfc4042f0
Josh Samuelson <josh@joshsamuelson.com>
2017-09-11 21:19:09 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,11 +1,11 @@
 class profile::ssh_server {
-  package {'openssh-server':
-    ensure => present,
-  }
-  service { 'sshd':
-    ensure => 'running',
-    enable => 'true',
-  }
+    package {'openssh-server':
+      ensure => present,
+    }
+    service { 'sshd':
+      ensure => 'running',
+      enable => 'true',
+    }
   ssh_authorized_key { 'root@master.puppet.vm':
     ensure => present,
     user   => 'root',
