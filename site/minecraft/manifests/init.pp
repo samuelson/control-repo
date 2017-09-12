e98fe5e9b7839b1fd497439eb72eb6b4b9bba6c9
Josh Samuelson <josh@joshsamuelson.com>
2017-09-11 22:08:49 -0700

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,32 @@
+class minecraft (
+  $game_mode = 1,
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
+  $install_dir = ‘/opt/minecraft’
+){
+  include wget
+  wget::fetch { "download minecraft":
+    source      => $minecraft_url,
+    destination => $install_dir',
+    timeout     => 0,
+    verbose     => false,
+  }
+  file { $install_dir:
+    ensure => directory,
+  }
+
+  package {‘java’:
+    ensure => present,
+  }
+  file {“${install_dir}/eula.txt”:
+    ensure => file,
+    content => ‘eula=true’
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
+    ensure => file,
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
+  }
+  service { ‘minecraft’:
+    ensure => running,
+    enable => true,
+  }
+}
